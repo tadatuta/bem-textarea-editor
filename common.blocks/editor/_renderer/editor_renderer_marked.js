344a395dc0cb82f18765e357932332b658ecf916
Vladimir Grinenko <i@tadatuta.com>
2018-09-02 23:35:10 +0300

    Update editor_renderer_marked.js

--- common.blocks/editor/_renderer/editor_renderer_marked.js
+++ common.blocks/editor/_renderer/editor_renderer_marked.js
@@ -1,6 +1,8 @@
 modules.define('editor', function(provide, Editor) {
 
 /* borschik:include:../../node_modules/marked/marked.min.js */
+    
+var marked = this.marked;
 
 provide(Editor.declMod({ modName: 'renderer', modVal: 'marked' }, {
     render: function() {
